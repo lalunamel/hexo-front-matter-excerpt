5487cb527af4b0fbaf11b22c95bed7e586a84f88
Kyle Baley <kyle@baley.org>
2015-12-14 18:25:02 -0500

    Pull excerpt from post content if not defined in front-matter or with --more--

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ var yaml = require('yaml-front-matter');
 
 hexo.extend.filter.register('after_post_render', function(data) {
 	var excerpt = yaml.loadFront(data.raw).excerpt;
+	var postExcerpt = data.content.substr(0,200);
 	if (excerpt !== undefined){
 		data.excerpt = excerpt;
+	} else if (postExcerpt !== undefined && (data.excerpt == undefined || data.excerpt.trim().length == 0)){
+		data.excerpt = postExcerpt + "...";
 	}
 	return data;
 });
